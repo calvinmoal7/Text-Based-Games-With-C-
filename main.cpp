8a31d59fd64f9bcd09005ba6e2af3c6f3a0e018e
calvin_moalosi <calvinm8@proton.me>
2026-05-09 21:12:49 +0200

    first commit - chapter 1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Lost Fortune RPG
+// A personalized adventure
+
+// libraries
+#include <iostream> // display to, and read text from console
+#include <string> // allows the creation of string objects
+
+using namespace std;
+
+int main() {
+    // define a constant
+    const int GOLD_PIECES = 900;
+
+    // declare variables
+    string leader; // last name of leader
+    int adventurers; // number of adventurers that begin the journey
+    int killed; // number of adventurers killed
+}
